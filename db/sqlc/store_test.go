95666900c9f6c8e3d42b3671b02f82d8efab47d9
dhirendraj-cmd <djha5051@gmail.com>
2026-09-25 20:04:30 +0530

    adding TDD with store test to get the transaaction flow

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,26 +2,29 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-
-type TxResultChan struct{
+type TxResultChan struct {
 	res TransferTxResult
 	err error
 }
 
-
-func TestTransferTx(t *testing.T){
+func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 
 	acc1 := CreateRandomAccount(t)
 	acc2 := CreateRandomAccount(t)
 
+	fmt.Println("Before Transaction >>>> ")
+	fmt.Println("Acc1 balance >>> ", acc1.Balance)
+	fmt.Println("Acc2 balance >>> ", acc2.Balance)
+
 	// run n concurrent transfers
-	n:=5
+	n := 15
 	amount := int64(10)
 
 	// errs := make(chan error)
@@ -29,12 +32,12 @@ func TestTransferTx(t *testing.T){
 
 	chres := make(chan TxResultChan, n)
 
-	for range n{
-		go func ()  {
+	for range n {
+		go func() {
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountId: acc1.ID,
-				ToAccountId: acc2.ID,
-				Amount: amount,
+				ToAccountId:   acc2.ID,
+				Amount:        amount,
 			})
 
 			chres <- TxResultChan{res: result, err: err}
@@ -42,11 +45,13 @@ func TestTransferTx(t *testing.T){
 	}
 
 	// check results
-	for range n{
+	existed := make(map[int]bool)
+
+	for range n {
 		out := <-chres
 		require.NoError(t, out.err)
 
-		res:=out.res
+		res := out.res
 		require.NotEmpty(t, res)
 
 		// check transfer
@@ -61,7 +66,6 @@ func TestTransferTx(t *testing.T){
 		_, err := store.GetTransfer(context.Background(), transfer.ID)
 		require.NoError(t, err)
 
-
 		// check entries
 		fromEntry := res.FromEntry
 		require.NotEmpty(t, fromEntry)
@@ -73,7 +77,6 @@ func TestTransferTx(t *testing.T){
 		_, err = store.GetEntry(context.Background(), fromEntry.ID)
 		require.NoError(t, err)
 
-
 		toEntry := res.ToEntry
 		require.NotEmpty(t, toEntry)
 		require.Equal(t, acc2.ID, toEntry.AccountID)
@@ -84,10 +87,56 @@ func TestTransferTx(t *testing.T){
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
+		// check accounts
+		fromAccount := res.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, acc1.ID, fromAccount.ID)
+		
+		toAccount := res.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, acc1.ID, toAccount.ID)
 
-		// check account balances
+		// check accounts balances
+		fmt.Println("at Transaction >>>> ")
+		fmt.Println("from Acc balance >>> ", fromAccount.Balance)
+		fmt.Println("to Acc balance >>> ", toAccount.Balance)
 
+		// diff bw input acc1 balance and output fromacc balance
+		diff1 := acc1.Balance - fromAccount.Balance
+
+		// diff bw input toacc balance and output acc2 balance
+		diff2 := toAccount.Balance - acc2.Balance
+
+		// if transaction is correct then diff1 n diff2 shoudl be same
+		require.Equal(t, diff1, diff2)
+
+		// diff should be positive
+		require.True(t, diff1 > 0)
+
+		// diff should be dvisible by amt of money in each transaction i.e for 1st it should be decreased by 1*amt, for 2nd 2*amt and soc on
+		require.True(t, diff1%amount==0)
+
+		k:=int(diff1/amount)
+		require.True(t, k>=1 && k<=n)
+
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
 
-}
+	// check final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), acc1.ID)
+	require.NoError(t, err)
 
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), acc2.ID)
+	require.NoError(t, err)
+
+	fmt.Println("After Transaction >>>> ")
+	fmt.Println("Acc1 balance >>> ", updatedAccount1.Balance)
+	fmt.Println("Acc2 balance >>> ", updatedAccount2.Balance)
+
+	require.Equal(t, acc1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, acc1.Balance+int64(n)*amount, updatedAccount2.Balance)
+
+
+
+}
--- util/random.go
+++ util/random.go
@@ -6,11 +6,9 @@ import (
 	"time"
 )
 
-
 const alphabets = "abcdefghijklmnopqrstuvwxyz"
 
-
-func init(){
+func init() {
 	rand.NewSource(time.Now().UnixNano())
 	// source := rand.NewSource(time.Now().UnixNano())
 	// localRand := rand.New(source)
@@ -18,37 +16,32 @@ func init(){
 
 // RandomInt generates a random integerer between min and max
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max - min + 1)
+	return min + rand.Int63n(max-min+1)
 }
-
 
 // random string generator
 func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabets)
 
-	for i:=0; i<n; i++{
-		c:=alphabets[rand.Intn(k)]
+	for range n {
+		c := alphabets[rand.Intn(k)]
 		sb.WriteByte(c)
 	}
 
 	return sb.String()
 }
 
-
 func RandOwner() string {
 	return RandomString(5)
 }
 
-
-func RandMoney() int64{
+func RandMoney() int64 {
 	return RandomInt(10, 10000)
 }
 
-
-func RandCurrency() string{
+func RandCurrency() string {
 	currencies := []string{"INR", "EUR", "USD"}
-	n:=len(currencies)
+	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
-
